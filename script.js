4ad006b5124e5f7b8cb49be59c91f78ef912eba8
Lukydnomo <andrioboffpv@gmail.com>
2025-03-11 16:05:38 -0300

    upd

---
+++ script.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var typed = new Typed("#texto", {
+        strings: ["O destino foi selado...", "Nada pode mudar o inevitável.", "Ele sempre esteve entre nós","só não havia sido descoberto ainda", "sabemos aonde ele está...", "né?", ".", "..", "..."],
+        typeSpeed: 65,  // Velocidade da digitação (quanto menor, mais rápido)
+        backSpeed: 15,  // Velocidade ao apagar
+        startDelay: 500, // Pequeno atraso antes de começar
+        backDelay: 1500, // Tempo antes de apagar o texto
+        loop: false,      // Repete a digitação
+        showCursor: true,
+        cursorChar: "_", // Caracter do cursor piscando
+    });
+});
+
+function abrirJanela() {
+    let resposta = prompt("").toLowerCase();
+    if (resposta === "humanidade morta") {
+        window.location.href = "finalidade.html"; // Substitua pela URL correta
+    } else {
+        alert("Errado.");
+    }
+}
