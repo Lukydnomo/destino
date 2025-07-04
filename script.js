df46ca146b6c4a9ececbd07cd31621ff2ed59f87
Lukydnomo <andrioboffpv@gmail.com>
2025-07-04 13:59:46 -0300

    update

--- script.js
+++ script.js
@@ -1,37 +1,48 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var typed = new Typed("#texto", {
-        strings: ["O destino foi selado...", "Nada pode mudar o inevitável.", "Ele sempre esteve entre nós","só não havia sido descoberto ainda", "sabemos aonde ele está...", "né?", ".", "..", "...", "eleestarala.html"],
-        typeSpeed: 65,  // Velocidade da digitação (quanto menor, mais rápido)
-        backSpeed: 15,  // Velocidade ao apagar
-        startDelay: 500, // Pequeno atraso antes de começar
-        backDelay: 1500, // Tempo antes de apagar o texto
-        loop: false,      // Repete a digitação
-        showCursor: true,
-        cursorChar: "_", // Caracter do cursor piscando
-    });
+document.addEventListener('DOMContentLoaded', function () {
+  var typed = new Typed('#texto', {
+    strings: [
+      'O destino foi selado...',
+      'Nada pode mudar o inevitável.',
+      'Ele sempre esteve entre nós',
+      'só não havia sido descoberto ainda',
+      'sabemos aonde ele está...',
+      'né?',
+      '.',
+      '..',
+      '...',
+      'eleestarala.html',
+    ],
+    typeSpeed: 65, // Velocidade da digitação (quanto menor, mais rápido)
+    backSpeed: 15, // Velocidade ao apagar
+    startDelay: 500, // Pequeno atraso antes de começar
+    backDelay: 1500, // Tempo antes de apagar o texto
+    loop: false, // Repete a digitação
+    showCursor: true,
+    cursorChar: '_', // Caracter do cursor piscando
+  });
 });
 
 function abrirJanela() {
-    let resposta = prompt("").toLowerCase();
-    if (resposta === "humanidade morta") {
-        window.location.href = "finalidade.html"; // Substitua pela URL correta
-    } else if (resposta === "eleestarala.html") {
-        window.location.href = "eleestarala.html";
-    } else {
-        alert("Errado.");
-    }
+  let resposta = prompt('').toLowerCase();
+  if (resposta === 'null') {
+    window.location.href = 'null.html'; // Substitua pela URL correta
+  } else {
+    alert('Errado.');
+  }
 }
 
 let currentPage = 1;
 
-document.querySelectorAll('.next-page').forEach(button => {
-    button.addEventListener('click', function() {
-        if (currentPage < 4) {
-            document.querySelector(`.page-${currentPage}`).style.opacity = 0;
-            document.querySelector(`.page-${currentPage}`).style.transform = 'rotateY(180deg)';
-            currentPage++;
-            document.querySelector(`.page-${currentPage}`).style.opacity = 1;
-            document.querySelector(`.page-${currentPage}`).style.transform = 'rotateY(0deg)';
-        }
-    });
+document.querySelectorAll('.next-page').forEach((button) => {
+  button.addEventListener('click', function () {
+    if (currentPage < 4) {
+      document.querySelector(`.page-${currentPage}`).style.opacity = 0;
+      document.querySelector(`.page-${currentPage}`).style.transform =
+        'rotateY(180deg)';
+      currentPage++;
+      document.querySelector(`.page-${currentPage}`).style.opacity = 1;
+      document.querySelector(`.page-${currentPage}`).style.transform =
+        'rotateY(0deg)';
+    }
+  });
 });
